c432cb758b9739b91c37505ee201ea64b8862e0f
Sashakrm <sasakramch@gmail.com>
2024-11-04 22:30:08 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("сторона: ");
+            double side = double.Parse(Console.ReadLine());
+            double perimeter = 4 * side; 
+            double area = side * side; 
+            Console.WriteLine($"Периметр: {perimeter}, Площадь: {area}");
+        }
+    }
+}
